5119296a1e780c97c2edc0a8d25e6e94645745c6
Your Name <you@example.com>
2026-03-23 22:26:12 +0530

    Force serve media files in production

--- spaceclub/config/urls.py
+++ spaceclub/config/urls.py
@@ -1,8 +1,8 @@
 import os
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 
 admin.site.site_header = "ASTEC Space Club Admin"
 admin.site.site_title = "ASTEC Space Club Admin Portal"
@@ -15,5 +15,7 @@ urlpatterns = [
     path('events/', include('events.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Force serving media files in production (Render)
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
